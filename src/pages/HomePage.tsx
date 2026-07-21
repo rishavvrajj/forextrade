740caf2334444f4f91c9110318afd07a8627f49c
rishavvrajj <rishav5raj3@gmail.com>
2026-07-22 04:58:29 +0530

    fix: correct hero section import path

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import HeroSection from '../views/heroSection'
+import HeroSection from '../views/HeroSection'
 
 export default function HomePage() {
   return (
     <div className='min-h-screen bg-black text-white'>
-      
+
       {/* Hero Section */}
         <HeroSection />
 
